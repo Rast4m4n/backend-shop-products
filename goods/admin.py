b098b121b1f964656178053bc15fbb88f53c7ba2
Kirill <motovilink@yandex.ru>
2023-03-09 18:51:03 +0300

    Добавил несколько таблиц баз данных

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Goods
+from .models import *
 
 
 class GoodsAdmin(admin.ModelAdmin):
@@ -7,5 +7,18 @@ class GoodsAdmin(admin.ModelAdmin):
     list_filter = ('favoriteGoods',)
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ('id', 'name',)
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'phoneNumber', 'name', 'mail')
+    list_filter = ('id', 'name',)
+
+
 admin.site.register(Goods, GoodsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Order)
+admin.site.register(FavoriteGoods)
 
--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Goods(models.Model):
     nameGoods = models.CharField(max_length=100)
     compositionOfGoods = models.TextField()
@@ -8,7 +15,7 @@ class Goods(models.Model):
     pathImage = models.TextField(null=True)
     priceGoods = models.IntegerField()
     favoriteGoods = models.BooleanField(default=False)
-    category = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.nameGoods} {self.priceGoods}₽ {self.weightGoods} грамм'
@@ -16,3 +23,38 @@ class Goods(models.Model):
     class Meta:
         verbose_name = "Goods"
         verbose_name_plural = "Goods"
+
+
+class User(models.Model):
+    phoneNumber = models.CharField(max_length=11, null=False)
+    name = models.CharField(max_length=100, null=True)
+    mail = models.CharField(max_length=100, null=True)
+    street = models.CharField(max_length=100, null=True)
+    entrance = models.CharField(max_length=2, null=True) # подъезд
+    floor = models.CharField(max_length=2, null=True)
+    flat = models.CharField(max_length=3, null=True)
+
+    def __str__(self):
+        return f'{self.phoneNumber} {self.name}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    countOfGoods = models.IntegerField()
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} {self.goods} {self.time}'
+
+
+class FavoriteGoods(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    isFavorite = models.BooleanField(null=False)
+
+    def __str__(self):
+        return f'{self.user} {self.goods} {self.isFavorite}'
+
+    class Meta:
+        verbose_name_plural = 'Favorite Goods'
